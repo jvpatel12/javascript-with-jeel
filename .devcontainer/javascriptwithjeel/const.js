5a4ea79dac04ecaf4479187b994aa2788023414f
jvpatel12 <148254662+jvpatel12@users.noreply.github.com>
2024-04-04 17:54:03 +0000

    container config done

---
+++ .devcontainer/javascriptwithjeel/const.js
@@ -0,0 +1,14 @@
+const account_Id=14522
+let accountEmail="jva2@gmail.com"
+var accountPassword="525252"
+accountCity="surat"
+
+
+//account_Id=5
+accountEmail="patel@bbb"
+accountPassword="552452"
+accountCity="vadodara"
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity])
+
+
---
+++ .devcontainer/javascriptwithjeel/datatypes.js
@@ -0,0 +1,7 @@
+//"use strict";
+//alert(3+3)
+console.log(3+3);
+let name="jeel"
+let age=19
+console.log(typeof name)
+console.log(typeof age)
